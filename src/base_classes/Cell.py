4cca3bd9f35d95478bc6ffaf0c247ea86c4199d1
m-machala <114995208+m-machala@users.noreply.github.com>
2025-07-13 14:13:02 +0200

    remove simulator

--- src/base_classes/Cell.py
+++
@@ -1,8 +0,0 @@
-# This class represents a generic cell. It contains references to its brain and data classes.
-class Cell:
-    def __init__(self, cellBrain):
-        self.cellData = {}
-        self.cellBrain = cellBrain
-
-    def execute(self):
-        self.cellBrain.run()
