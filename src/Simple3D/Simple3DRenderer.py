7000d4025880454b67d79d5cf000830287384177
m-machala <114995208+m-machala@users.noreply.github.com>
2025-07-31 15:39:24 +0200

    add rotation

--- src/Simple3D/Simple3DRenderer.py
+++ src/Simple3D/Simple3DRenderer.py
@@ -27,8 +27,9 @@ class Simple3DRenderer(Renderer):
         super().__init__(outputResolutionW, outputResolutionH)
 
         self._cameraPosition = [0.0, 0.0, 0.0] # X, Y, Z
-        self._cameraRotation = [0.0, 0.0, 0.0] # roll, pitch, yaw 
+        self._cameraRotationMatrix = np.identity(3)  # pitch, yaw, roll 
         self._cameraMovementSpeed = 0.25
+        self._cameraRotationSpeed = 0.05
 
         self.test1 = 0
         self.test2 = 0
@@ -41,6 +42,12 @@ class Simple3DRenderer(Renderer):
                                  ExportFunction(self._moveRight, "Move right", ControlElement.REPEATINGBUTTON, [10]),
                                  ExportFunction(self._moveForward, "Move forward", ControlElement.REPEATINGBUTTON, [10]),
                                  ExportFunction(self._moveBack, "Move back", ControlElement.REPEATINGBUTTON, [10]),
+                                 ExportFunction(self._rotateUp, "Rotate up", ControlElement.REPEATINGBUTTON, [10]),
+                                 ExportFunction(self._rotateDown, "Rotate down", ControlElement.REPEATINGBUTTON, [10]),
+                                 ExportFunction(self._rotateLeft, "Rotate left", ControlElement.REPEATINGBUTTON, [10]),
+                                 ExportFunction(self._rotateRight, "Rotate right", ControlElement.REPEATINGBUTTON, [10]),
+                                 ExportFunction(self._rotateClockwise, "Rotate clockwise", ControlElement.REPEATINGBUTTON, [10]),
+                                 ExportFunction(self._rotateCounterclockwise, "Rotate counterclockwise", ControlElement.REPEATINGBUTTON, [10]),
                                  ExportFunction(self._changeFOV, "FOV", ControlElement.SLIDER, [1, 1799, 900])]
 
         self._backgroundColor = (0, 0, 0)
@@ -106,13 +113,21 @@ class Simple3DRenderer(Renderer):
             skip = False
             for vertexIndex in quad:
                 vertex = cubeVertices[vertexIndex]
-                distance = vertex[2] + coordinates[2] - self._cameraPosition[2]
-                if distance <= 0:
+                x = vertex[0] + coordinates[0] - self._cameraPosition[0]
+                y = vertex[1] + coordinates[1] - self._cameraPosition[1]
+                z = vertex[2] + coordinates[2] - self._cameraPosition[2]
+
+                transformationMatrix = self._cameraRotationMatrix
+                vector = np.array([x, y, z])
+                transformed = vector @ transformationMatrix
+                x, y, z = np.asarray(transformed).flatten()
+
+                if z <= 0:
                     skip = True
                     break
-                width = self.outputResolutionW / self._getBaseFromAngleAndHeight(self._FOV, distance)
-                x = screenCenterX + width * (vertex[0] + coordinates[0] - self._cameraPosition[0])
-                y = screenCenterY + width * (vertex[1] + coordinates[1] - self._cameraPosition[1])
+                width = self.outputResolutionW / self._getBaseFromAngleAndHeight(self._FOV, z)
+                x = screenCenterX + width * x
+                y = screenCenterY + width * y
 
                 polygon.append((x, y))
             if not skip:
@@ -125,27 +140,78 @@ class Simple3DRenderer(Renderer):
             return 1
         
         return np.tan(np.radians(angle) / 2) * height * 2
+
     
     def convertFromImageCoordinates(self, xCoordinate, yCoordinate):
         pass
 
-    def _moveDown(self):
-        self._cameraPosition[1] -= self._cameraMovementSpeed
+    def _moveCamera(self, movementVector):
+
+        transformationMatrix = self._cameraRotationMatrix.T
+        vector = np.array(movementVector) * self._cameraMovementSpeed
+        transformed = vector @ transformationMatrix
+        x, y, z = np.asarray(transformed).flatten()
+
+        self._cameraPosition[0] += x
+        self._cameraPosition[1] += y
+        self._cameraPosition[2] += z
 
     def _moveUp(self):
-        self._cameraPosition[1] += self._cameraMovementSpeed
+        self._moveCamera([0, -1, 0])
+
+    def _moveDown(self):
+        self._moveCamera([0, 1, 0])
 
     def _moveLeft(self):
-        self._cameraPosition[0] -= self._cameraMovementSpeed
+        self._moveCamera([-1, 0, 0])
 
     def _moveRight(self):
-        self._cameraPosition[0] += self._cameraMovementSpeed
+        self._moveCamera([1, 0, 0])
 
     def _moveBack(self):
-        self._cameraPosition[2] -= self._cameraMovementSpeed
+        self._moveCamera([0, 0, -1])
 
     def _moveForward(self):
-        self._cameraPosition[2] += self._cameraMovementSpeed
+        self._moveCamera([0, 0, 1])
+
+
+    def _rotateCamera(self, rotationVector):
+        pitch, yaw, roll = np.array(rotationVector) * self._cameraRotationSpeed
+
+        pitchMatrix = np.array([[1, 0, 0],
+                                [0, np.cos(pitch), -np.sin(pitch)],
+                                [0, np.sin(pitch), np.cos(pitch)]])
+
+        yawMatrix = np.array([[np.cos(yaw), 0, np.sin(yaw)],
+                              [0, 1, 0],
+                              [-np.sin(yaw), 0, np.cos(yaw)]])
+
+        rollMatrix = np.array([[np.cos(roll), -np.sin(roll), 0],
+                               [np.sin(roll),  np.cos(roll), 0],
+                               [0, 0, 1]])
+
+        deltaRotation = yawMatrix @ pitchMatrix @ rollMatrix
+
+        self._cameraRotationMatrix = self._cameraRotationMatrix @ deltaRotation
+
+
+    def _rotateLeft(self):
+        self._rotateCamera([0, -1, 0])
+
+    def _rotateRight(self):
+        self._rotateCamera([0, 1, 0])
+
+    def _rotateDown(self):
+        self._rotateCamera([-1, 0, 0])
+
+    def _rotateUp(self):
+        self._rotateCamera([1, 0, 0])
+
+    def _rotateCounterclockwise(self):
+        self._rotateCamera([0, 0, -1])
+
+    def _rotateClockwise(self):
+        self._rotateCamera([0, 0, 1])
 
     def _changeFOV(self, FOV):
         self._FOV = FOV / 10
